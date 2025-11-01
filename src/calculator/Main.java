5d99626ca3f7f5db4d66e8f034c304fcea051d18
Herstocher <tekorcun@gmail.com>
2025-11-02 00:01:19 +0100

    Testing Git on Eclipse IDE

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Window window = new Window();
+		
+		int zahl1 = 1;
+		int zahl2 = 2;
+		int zahl3 = 3;
 	}
 
 }
