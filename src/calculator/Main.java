faeedce96bc8c56fb89722e1a599dfaf9eae09a1
Herstocher <tekorcun@gmail.com>
2025-11-02 00:37:28 +0100

    Setting up the packages needed + sorting out Git issues

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -4,11 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Window window = new Window();
-		
-		int zahl1 = 1;
-		int zahl2 = 2;
-		int zahl3 = 3;
+		new Window();
 	}
 
 }
--- src/calculator/Window.java
+++ src/calculator/Window.java
@@ -1,4 +1,8 @@
 package calculator;
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.math.*;
 
 public class Window {
 
